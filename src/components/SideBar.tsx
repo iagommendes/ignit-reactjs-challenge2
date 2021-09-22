c95d2621c5aa844d1af2f33348fbdc8f25ae687a
Iago Mendes <iagommendes@gmail.com>
2021-09-22 14:57:28 -0300

    creating sidebar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
-  // Complete aqui
+interface SideBarProps {
+  fisrtTitle: string;
+  secondTitle: string;
+  children: Array<any>;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        {props.fisrtTitle}
+        <p>{props.secondTitle}</p>
+      </span>
+
+      <div className="buttons-container">{props.children}</div>
+    </nav>
+  );
 }
