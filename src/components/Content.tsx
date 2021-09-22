3b27b916a135dbb97ef46dec555f4b2b4eabcaf4
Iago Mendes <iagommendes@gmail.com>
2021-09-22 14:58:30 -0300

    creating content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,13 @@
-export function Content() {
-  // Complete aqui
+interface ContentProps {
+  children: Array<any>;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {props.children}
+      </div>
+    </main>
+  );
 }
