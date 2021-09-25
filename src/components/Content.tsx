2af4e9d1dfb8cd39c9e7156bf590880eadd5dd7b
Iago Mendes <iagommendes@gmail.com>
2021-09-25 17:34:30 -0300

    Update Content.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,13 +1,43 @@
+import { MovieCard } from "./MovieCard";
+
 interface ContentProps {
-  children: Array<any>;
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+  selectedGenre: {title: string}
 }
 
-export function Content(props: ContentProps) {
+export function Content({ movies, selectedGenre }: ContentProps) {
+  console.log(selectedGenre)
   return (
-    <main>
-      <div className="movies-list">
-        {props.children}
-      </div>
-    </main>
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+          
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
